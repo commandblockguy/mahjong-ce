f94094b70b804bfab752f0b622e3b7a97f1d005f
commandblockguy <commandblockguy1@gmail.com>
2019-10-28 10:32:02 -0500

    Fixed a bug where tiles on the edges of the map can't be removed

--- src/tile.c
+++ src/tile.c
@@ -18,9 +18,9 @@
 /* Get the type of a tile (zeroing the offset bits) */
 uint8_t get_type(uint8_t x, uint8_t y, uint8_t z) {
 	/* Check if position is valid */
-	if(x > TILES_X) return NO_TILE;
-	if(y > TILES_Y) return NO_TILE;
-	if(z > TILES_Z) return NO_TILE;
+	if(x >= TILES_X) return NO_TILE;
+	if(y >= TILES_Y) return NO_TILE;
+	if(z >= TILES_Z) return NO_TILE;
 
 	return game.tiles[x][y][z] & bmTileType;
 }
